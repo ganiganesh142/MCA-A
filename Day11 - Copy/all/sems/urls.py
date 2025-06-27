62def99868ef1f5597d4108fbe0ac412d7c667f2
ganiganesh142 <ganiganesh143n@gmail.com>
2025-06-27 16:04:22 +0530

    Add files via upload

---
+++ Day11 - Copy/all/sems/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from. import views
+urlpatterns= [
+	path('sem1/',views.sem1, name='sem1'),
+]
---
+++ Day11 - Copy/all/sems/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpsResponse
+def sem1(request):
+    return HttpResponse("sems")
+
+# Create your views here.
